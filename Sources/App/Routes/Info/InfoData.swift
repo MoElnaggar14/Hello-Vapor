53ac8f8dfcf9e0729f25b158706c6067c206c6f2
MoElnaggar14 <moelnaggar14@gmail.com>
2020-09-10 14:32:27 +0200

    intial Vapor

---
+++ Sources/App/Routes/Info/InfoData.swift
@@ -0,0 +1,13 @@
+//
+//  InfoData.swift
+//  
+//
+//  Created by Mohammed Elnaggar on 9/10/20.
+//
+
+import Foundation
+import Vapor
+
+struct InfoData: Content {
+    let name: String
+}
---
+++ Sources/App/Routes/Info/InfoResponse.swift
@@ -0,0 +1,13 @@
+//
+//  InfoResponse.swift
+//  
+//
+//  Created by Mohammed Elnaggar on 9/10/20.
+//
+
+import Foundation
+import Vapor
+
+struct InfoResponse: Content {
+    let data: InfoData
+}
